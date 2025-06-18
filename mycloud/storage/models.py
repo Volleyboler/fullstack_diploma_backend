037da499fea7d9e8f427c1b5d7d882297c78c55f
Ilia Tregub <forgotten-devil@mail.ru>
2025-06-19 01:40:43 +0500

    models added

--- mycloud/storage/models.py
+++ mycloud/storage/models.py
@@ -1,1 +1,23 @@
+import uuid
+import os
 from django.db import models
+from django.conf import settings
+
+def generate_file_path(instance, filename):
+    ext = os.path.splitext(filename)[1]
+    unique_filename = f"{uuid.uuid4()}{ext}"
+    return os.path.join('uploads', str(instance.user.id), unique_filename)
+
+
+class File(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    original_name = models.CharField(max_length=255)
+    file_size = models.PositiveIntegerField()
+    upload_date = models.DateTimeField(auto_now_add=True)
+    last_download = models.DateTimeField(null=True, blank=True)
+    comment = models.TextField(blank=True)
+    file_path = models.FileField(upload_to=generate_file_path)
+    share_link = models.CharField(max_length=100, unique=True, default=uuid.uuid4)
+
+    def __str__(self):
+        return self.original_name
--- mycloud/users/models.py
+++ mycloud/users/models.py
@@ -1,1 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, full_name, password=None):
+        if not email:
+            raise ValueError('Email обязателен')
+        if not username:
+            raise ValueError('Логин обязателен')
+        if not password:
+            raise ValueError('Пароль обязателен')
+
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+            full_name=full_name,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, full_name, password=None):
+        user = self.create_user(username, email, full_name, password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=20, unique=True)
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    is_admin = models.BooleanField(default=False)
+    storage_path = models.CharField(max_length=255, blank=True, null=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'full_name']
+
+    @property
+    def is_staff(self):
+        return self.is_admin
